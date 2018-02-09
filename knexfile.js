19677389552a9f80869b694038c26b4d1226eef4
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-09 10:46:45 -0700

    first push

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql:///memorydrill3'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
