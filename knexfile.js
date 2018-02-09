6d1929ec9f00ddbbcf73224612226a79bb68add2
nsolimine <32491766+nsolimine@users.noreply.github.com>
2018-02-09 13:34:29 -0700

    heroku updates

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-// Update with your config settings.
+require("dotenv").load();
 
 module.exports = {
 
@@ -9,6 +9,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
 };
